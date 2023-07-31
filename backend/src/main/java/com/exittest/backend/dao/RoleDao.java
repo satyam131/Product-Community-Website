c39f0cfbd980ebd09ea8e022523526666abac909
Satyam Kumar Prajapati <73646662+satyam131@users.noreply.github.com>
2023-07-31 11:58:16 +0530

    Delete backend directory

--- backend/src/main/java/com/exittest/backend/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.exittest.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exittest.backend.model.Role;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-
-}
